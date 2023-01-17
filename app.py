ee3fadab75e8476d6c72833795f563e324433aa7
harithushan3 <harithushan3@gmail.com>
2023-01-17 19:57:56 +0530

    modified the app return text on the web page

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "CI CD pipeline has been established."
+    return """CI CD pipeline has been established.
+    just modified the file adian 
+    so i have reached the level succesfully 
+    am so excited
+    love you"""
 
 
 if __name__=="__main__":
