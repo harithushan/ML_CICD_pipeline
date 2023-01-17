70af0dc2a76692bcc5aa57db767671ae5bb15962
harithushan3 <harithushan3@gmail.com>
2023-01-17 23:01:47 +0530

    updated files

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 #Declaring  variables for setup function
-Project_name ="housing predictor"
+Project_name ="housingpredictor"
 Version = "0.0.1"
 Author ="Paramanathan Thushanthan"
 Description = "This is my machine learning project on housing prediction during the course study"
@@ -16,14 +16,14 @@ def get_requirements_list()->List[str]:
     return  this function is goin to return a list which contains name of the libraries  mentioned in the requirements.txt 
     """
     with open(Requirements_file_name)as requirement_file :
-        requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .") #removing "-e ." here because find_packages() will also do the job of this
 
-
+# packages=find_packages()# can also be used
 setup(
-    name=Project_name,
-    version=Version,
-    author=Author,
-    description=Description,
-    packages=Packages,
-    install_requires =get_requirements_list()
+name=Project_name,
+version=Version,
+author=Author,
+description=Description,
+packages=find_packages()#Packages#["housing"]
+install_requires =get_requirements_list()
 )
