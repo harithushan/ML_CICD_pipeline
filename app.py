b79abb4021d3c7f6db74a0ccfb159e9e78c5e7f8
harithushan3 <harithushan3@gmail.com>
2023-01-18 21:46:25 +0530

    modified the logging and exception files

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("We are testing the logging module")
+    try:
+        raise Exception("We are testing the logging module")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        # raise HousingException(e,sys) from e
+        logging.info(housing.error_message)
+        logging.info("We are testing the logging module")
     return "CICD pipeline has been established"
 
 
